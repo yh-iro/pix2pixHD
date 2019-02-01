b0149a31819e8dacb9ef592d2f940b17569e4877
yh-iro <yhirota@ruri.waseda.jp>
2019-02-02 02:59:43 +0900

    bug fix about cuda check make FaceSwapGenerator singleton

--- models/networks.py
+++ models/networks.py
@@ -39,7 +39,7 @@ def define_G(input_nc, output_nc, ngf, netG, n_downsample_global=3, n_blocks_glo
     else:
         raise('generator not implemented!')
     print(netG)
-    if gpu_ids[0] > 0:
+    if gpu_ids[0] >= 0:
         assert(torch.cuda.is_available())   
         netG.cuda(gpu_ids[0])
     netG.apply(weights_init)
@@ -49,7 +49,7 @@ def define_D(input_nc, ndf, n_layers_D, norm='instance', use_sigmoid=False, num_
     norm_layer = get_norm_layer(norm_type=norm)   
     netD = MultiscaleDiscriminator(input_nc, ndf, n_layers_D, norm_layer, use_sigmoid, num_D, getIntermFeat)   
     print(netD)
-    if gpu_ids[0] > 0:
+    if gpu_ids[0] >= 0:
         assert(torch.cuda.is_available())
         netD.cuda(gpu_ids[0])
     netD.apply(weights_init)
